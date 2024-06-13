9c4e7dd9244169980e0e3d45eb9f79337aafe64f
Mariem <mariemmohamed1421@gmail.com>
2024-06-13 15:36:57 +0300

    Preparing Order Tables

---
+++ Cooklee.Data/Entities/Order/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cooklee.Data.Entities.Order
+{
+    public class Order
+    {
+    }
+}
