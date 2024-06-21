efb1c261e8bc3c175af163a2918ed082c289e287
Mariem <mariemmohamed1421@gmail.com>
2024-06-21 16:28:25 +0300

    FavCart Components

--- Cooklee.API/Controllers/FavouriteItemController.cs
+++ Cooklee.API/Controllers/FavouriteItemController.cs
@@ -17,29 +17,46 @@ namespace Cooklee.API.Controllers
             _unit = unit;
         }
 
+        //[HttpPost]
+        //public async Task<ActionResult<ClientFavourite>> AddFavouriteItem(string favouriteId, FavouriteItem item)
+        //{
+        //    if (_unit.MealRepository.GetAsync(item.Id) != null)
+        //    {
+        //        var favourite = await _unit.FavoriteRepository.AddFavouriteItem(favouriteId, item);
+        //        if (favourite.Items.Any(i => i.Id == item.Id))
+        //        {
+        //            return BadRequest("Item already exists in favorites.");
+        //        }
+
+        //        if (favourite != null)
+        //        {
+        //            return Ok(favourite);
+        //        }
+        //        return Ok("You can not add more than 10!!");
+        //    }
+        //    else
+        //    {
+        //        return BadRequest("You cannot add a meal that does not exist!!!");
+        //    }
+        //}
+
         [HttpPost]
         public async Task<ActionResult<ClientFavourite>> AddFavouriteItem(string favouriteId, FavouriteItem item)
         {
             if (_unit.MealRepository.GetAsync(item.Id) != null)
             {
                 var favourite = await _unit.FavoriteRepository.AddFavouriteItem(favouriteId, item);
-                if (favourite.Items.Any(i => i.Id == item.Id))
-                {
-                    return BadRequest("Item already exists in favorites.");
-                }
-
                 if (favourite != null)
                 {
                     return Ok(favourite);
                 }
-                return Ok("You can not add more than 10!!");
+                return Ok("You cannot add the same meal more than once!!");
             }
             else
             {
                 return BadRequest("You cannot add a meal that does not exist!!!");
             }
         }
-
 
         [HttpDelete("{favouriteId}")]
         public async Task<ActionResult<ClientFavourite>> DeleteFavouriteItem(string favouriteId, [FromBody] FavouriteItem item)
