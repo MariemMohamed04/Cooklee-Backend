3d41dcc5328af21a6d298453aa90f3d0180b4f1e
Mariem <mariemmohamed1421@gmail.com>
2024-06-22 15:35:45 +0300

    Adding Columns to Shipment Details

---
+++ Cooklee.Infrastructure/Repositories/ShipmentDetailsRepo.cs
@@ -0,0 +1,17 @@
+﻿using Cooklee.Data.Entities.Order;
+using Cooklee.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cooklee.Infrastructure.Repositories
+{
+    //public class ShipmentDetailsRepo : GenericRepo<ShipmentDetails>
+    //{
+    //    public ShipmentDetailsRepo(CookleeDbContext dbcontext) : base(dbcontext)
+    //    {
+    //    }
+    //}
+}
