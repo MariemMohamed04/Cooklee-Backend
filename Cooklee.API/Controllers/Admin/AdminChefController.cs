7df7259d37d6885e0ca0d22bb0d0c08e894c4a31
Mariem <mariemmohamed1421@gmail.com>
2024-06-29 13:22:21 +0300

    Test

---
+++ Cooklee.API/Controllers/Admin/AdminChefController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cooklee.API.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminChefController : BaseApiController
+    {
+        //
+    }
+}
